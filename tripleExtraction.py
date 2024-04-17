41e3262cb12921a55f2beeba667fd114e42bfedc
ABDULLA AHMADKHAN <e2349611@ceng.metu.edu.tr>
2024-04-18 00:44:23 +0500

    Fixed triple extraction memory issue

--- tripleExtraction.py
+++ tripleExtraction.py
@@ -11,46 +11,39 @@ import pickle
         self.relation = relation
         self.pmid = pmid '''                                       # PubMed ID of the article
 
-def getTripletsFromCTR(root):   
+def getTripletsFromCTD(xml_path):
     triplets = {}
-    for row in root.findall('Row'):
-        chemical = row.find('ChemicalID').text
-        disease = row.find('DiseaseID').text.split(':')[1]
-        pmid = row.find('PubMedIDs').text if row.find('PubMedIDs') is not None else 'NIL'
-        relation = 'THR' if row.find('DirectEvidence') is 'therapeutic' else 'NIL'
-        relation = 'INF' if row.find('InferenceScore') is not None else 'NIL'
-        relation = 'CID' if row.find('DirectEvidence') is 'marker/mechanism' else 'NIL'
-        key = (chemical, disease, pmid)
-        if key in triplets:
-            triplets[key] = triplets[key].append(relation)
-        else:
-            triplets[key] = [relation]
+    for _, elem in ET.iterparse(xml_path, events=('end',)):
+        if elem.tag == 'Row':
+            chemical = elem.find('ChemicalID').text
+            disease = elem.find('DiseaseID').text.split(':')[1] if elem.find('DiseaseID') is not None else 'NIL'
+            pmid = elem.find('PubMedIDs').text if elem.find('PubMedIDs') is not None else 'NIL'
+            relation = 'THR' if elem.find('DirectEvidence') is 'therapeutic' else 'NIL'
+            relation = 'INF' if elem.find('InferenceScore') is not None else 'NIL'
+            relation = 'CID' if elem.find('DirectEvidence') is 'marker/mechanism' else 'NIL'
+            key = (chemical, disease, pmid)
+            triplets.setdefault(key, []).append(relation)
+            elem.clear()
+
     return triplets
 
 def getTripletsFromCDR(docs, tripletsCTD):
     triplets = {}
-    for doc in docs:                   # Pairs in specific doc
+    for doc in docs:
         for triplet in doc.relations:
             key = (triplet.pmid, triplet.chemical.id, triplet.disease.id)
             if key in tripletsCTD:
-                if key in triplets:
-                    triplets[key] = triplets[key].append(triplet.relation.type)
-                else:
-                    triplets[key] = [triplet.relation.type]
+                triplets.setdefault(key, []).append(triplet.relation.type)
             else:
-                triplets[key] = 'NIL'
+                triplets[key] = ['NIL']
     return triplets
 
 if __name__ == '__main__':
-    
-    tree = ET.parse('./CTD_Data/CTD_chemicals_diseases.xml')
-    root = tree.getroot()
-    
-    # Create pairs from the loaded documents
-    tripletsCTD = getTripletsFromCTR(root)
-    # Load the document data from a file
+    tripletsCTD = getTripletsFromCTD('./CTD_Data/CTD_chemicals_diseases.xml')
+
     with open('./CDR_Data/CDR.Corpus.v010516/CDR_Training+TestSet.PubTator.txt', 'r') as fp:
         docs = pubtator.load(fp)
+
     tripletsCDR = getTripletsFromCDR(docs, tripletsCTD)
 
-    print(len(tripletsCTD) + len(tripletsCDR))
+    print(f"Total CTD Triplets: {len(tripletsCTD)}, Total CDR Triplets: {len(tripletsCDR)}")
